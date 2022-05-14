294af8a83c3908ad7b84d7db72f769cf70bf585d
Stefan Norinder <stefan.norinder@sh.se>
2022-05-14 18:15:32 +0200

    unit test simple data template

--- CodeGenerator.Test/CodeGenerator/CodeGeneratorTests.cs
+++ CodeGenerator.Test/CodeGenerator/CodeGeneratorTests.cs
@@ -17,5 +17,14 @@ namespace CodeGenerator.Test
             var code = new SimpleTestTemplate().TransformText();
             Assert.AreEqual(result, code);
         }
+
+        [Test]
+        public void GenerateCodeFromDataUsingSimpleDataTemplate()
+        {
+            var data = new string[] { "Foo", "Bar" };
+            const string result = "public namespace CodeGenerator.TestTemplates\r\n{\r\n    public class SimpleDataTest\r\n    {\r\n        public string Foo { get; set; }\r\n        public string Bar { get; set; }\r\n    }\r\n}";
+            var code = new SimpleDataTestTemplate(data).TransformText();
+            Assert.AreEqual(result, code);
+        }
     }
 }
---
+++ CodeGenerator.Test/TempateExtensions/SimpleTestTemplateExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGenerator.Test.TestFiles
+{
+    partial class SimpleDataTestTemplate
+    {
+        private string[] @params;
+
+        public SimpleDataTestTemplate(string[] @params)
+        {
+            this.@params = @params;
+        }
+
+
+    }
+}
