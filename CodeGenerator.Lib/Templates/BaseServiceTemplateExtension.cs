7f70a1da109057b880b57355b48f2905a721fcde
Stefan Norinder <stefan.norinder@sh.se>
2022-05-22 20:20:07 +0200

    base service template

---
+++ CodeGenerator.Lib/Templates/BaseServiceTemplateExtension.cs
@@ -0,0 +1,17 @@
+﻿using CodeGenerator.Lib.Models;
+
+namespace CodeGenerator.Lib.Templates
+{
+    partial class BaseServiceTemplate
+    {
+        public readonly string namespaceName;
+
+        public BaseServiceTemplate(string namespaceName)
+        {
+            this.namespaceName = namespaceName;
+            Model = new Class { Name = "Base" };
+        }
+
+        public Class Model { get; }
+    }
+}
