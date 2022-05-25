12ef7027c2fed4c55768e7b8150809e3686ef14c
Stefan Norinder <stefan.norinder@sh.se>
2022-05-25 13:59:11 +0200

    passing classes instead of class

---
+++ CodeGenerator.Lib/Templates/StartupTemplateExtension.cs
@@ -0,0 +1,18 @@
+﻿using CodeGenerator.Lib.Models;
+using System.Collections.Generic;
+
+namespace CodeGenerator.Lib.Templates
+{
+    partial class StartupTemplate
+    {
+        public readonly string namespaceName;
+
+        public StartupTemplate(string namespaceName, IEnumerable<Class> @class)
+        {
+            this.namespaceName = namespaceName;
+            Model = @class;
+        }
+
+        public IEnumerable<Class> Model { get; }
+    }
+}
